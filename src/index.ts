4400c3bdd269559b2bf2aa92470146d8114bfe58
Stephen Chung <spchung95@gmail.com>
2025-07-05 15:49:36 -0400

    command

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,13 @@
-import _ from "lodash";
-import type { Queen, Season } from "./types";
-import { processShows } from "./scrape/scrapeShows";
-import { processQueens } from "./scrape/scrapeQueens";
+import ScrapeQueensCommand from "./scrape/command";
 
 const BASE_FANDOM_URL = "https://rupaulsdragrace.fandom.com";
-
 const SHOWS_TO_PROCESS = ["RuPaul's Drag Race"];
 
 async function main() {
-  console.log(`# of shows to process: ${SHOWS_TO_PROCESS.length}\n`);
-
-  const seasons: Season[] = [];
-  await Promise.all(
-    _.chunk(SHOWS_TO_PROCESS, 5).map(async (shows) => {
-      const batchResults = await processShows(BASE_FANDOM_URL, shows);
-      seasons.push(...batchResults);
-    })
-  );
-
-  const uniqueQueens = _.uniqBy(
-    seasons.flatMap((s) => s.contestants),
-    "name"
-  );
-
-  console.log(`\n# of queens to process: ${uniqueQueens.length}\n`);
-  const queens: Queen[] = [];
-  await Promise.all(
-    _.chunk(uniqueQueens, 50).map(async (qs) => {
-      const batchResults = await processQueens(qs);
-      queens.push(...batchResults);
-    })
-  );
-
-  queens.sort((a, b) => a.name.localeCompare(b.name));
+  const queens = await new ScrapeQueensCommand(
+    BASE_FANDOM_URL,
+    SHOWS_TO_PROCESS
+  ).execute();
 
   await Bun.write("rpdr_queens.json", JSON.stringify(queens, null, 2));
 
---
+++ src/scrape/command.ts
@@ -0,0 +1,43 @@
+import type { Queen, Season } from "../types";
+import _ from "lodash";
+import { processQueens } from "./scrapeQueens";
+import { processShows } from "./scrapeShows";
+
+class ScrapeQueensCommand {
+  private shows: string[];
+
+  constructor(private readonly baseUrl: string, shows: string[]) {
+    this.shows = shows;
+  }
+
+  async execute() {
+    console.log(`# of shows to process: ${this.shows.length}\n`);
+    const seasons: Season[] = [];
+    await Promise.all(
+      _.chunk(this.shows, 5).map(async (shows) => {
+        const batchResults = await processShows(this.baseUrl, shows);
+        seasons.push(...batchResults);
+      })
+    );
+
+    const uniqueQueens = _.uniqBy(
+      seasons.flatMap((s) => s.contestants),
+      "name"
+    );
+
+    console.log(`\n# of queens to process: ${uniqueQueens.length}\n`);
+    const queens: Queen[] = [];
+    await Promise.all(
+      _.chunk(uniqueQueens, 50).map(async (qs) => {
+        const batchResults = await processQueens(qs);
+        queens.push(...batchResults);
+      })
+    );
+
+    queens.sort((a, b) => a.name.localeCompare(b.name));
+
+    return queens;
+  }
+}
+
+export default ScrapeQueensCommand;
